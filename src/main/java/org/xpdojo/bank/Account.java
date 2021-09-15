9b90f8661e8124392e7d4f9be39e756e8bbfef29
jaiparkash.singh@gmail.com <Maina@1313>
2021-09-15 17:36:34 +0530

    First Test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,9 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public int balance ;
+    public int deposit(int amount){
+        this.balance += amount;
+        return balance;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -6,10 +6,25 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
+    @Test
+    public void startBalanceIsZero() {
+        Account account = new Account();
+        assertThat( account.balance).isEqualTo(0);
+    }
+    @Test
+//	@Disabled
+    public void depositAnAmountToIncreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        assertThat( account.balance).isEqualTo(10);
+    }
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+//	@Disabled
+    public void depositMultipleAnAmountToIncreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(100);
+        assertThat( account.balance).isEqualTo(110);
     }
 }
